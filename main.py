6eda2c4f8e3a76f3d5598c7a73abd45834fc38e8
Divinenano <kyleschindel@outlook.com>
2025-08-31 20:27:05 -0700

    E: Finished bookbot

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-from stats import get_num_words, total_counter, chars_dict_to_sorted_list
+import sys
 
-path = "books/frankenstein.txt"
+from stats import get_num_words, total_counter, chars_dict_to_sorted_list
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    path = sys.argv[1]
 
 counts = total_counter(path)
 sorted_list = chars_dict_to_sorted_list(counts)
 count = get_num_words(path)
 
 print("============ BOOKBOT ============")
-print("Analyzing book found at books/frankenstein.txt...")
+print(f"Analyzing book found at {path}...")
 print("----------- Word Count ----------")
 print(f"Found {count} total words")
 print("--------- Character Count -------")
 for item in sorted_list:
     print(f"{item['char']}: {item['num']}")
 print("============= END ===============")
-
