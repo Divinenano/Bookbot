1e4f44b4a6707b8e8dfbf78d608b188347d7342a
Divinenano <kyleschindel@outlook.com>
2025-08-31 19:17:26 -0700

    D: The report

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-from stats import get_num_words
+from stats import get_num_words, total_counter, chars_dict_to_sorted_list
+
+path = "books/frankenstein.txt"
+
+counts = total_counter(path)
+sorted_list = chars_dict_to_sorted_list(counts)
+count = get_num_words(path)
+
+print("============ BOOKBOT ============")
+print("Analyzing book found at books/frankenstein.txt...")
+print("----------- Word Count ----------")
+print(f"Found {count} total words")
+print("--------- Character Count -------")
+for item in sorted_list:
+    print(f"{item['char']}: {item['num']}")
+print("============= END ===============")
+
--- stats.py
+++ stats.py
@@ -3,11 +3,29 @@ def get_num_words(filepath):
         file_contents = f.read()
         words = str.split(file_contents)
         word_count = len(words)
-    return (f"{word_count} words found in the document")
+    return word_count
 
-def stats():
-    path = "books/frankenstein.txt"
-    text = get_num_words(path)
-    print(text)
+def total_counter(filepath):
+    dictionary = {}
+    with open(filepath) as f:
+        file_contents = f.read()
+        for char in file_contents:
+            lowcase_char = str.lower(char)
+            if lowcase_char in dictionary:
+                dictionary[lowcase_char] += 1
+            else:
+                dictionary[lowcase_char] = 1
+    return dictionary
 
-stats()
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        if not ch.isalpha():
+            continue
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
